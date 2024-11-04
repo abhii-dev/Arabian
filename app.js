ca7461da329a9b1a2c743e269fa8f940fb2a4df6
Moheenkhan <moheenkhanpersonal@gmail.com>
2024-11-04 22:44:44 +0530

    pushed

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
+// js for login page
+
+formContainer = document.querySelector(".form-container");
+loginA = document.querySelector(".loginA");
+cross = document.querySelector(".cross");
+
+loginA.addEventListener("click", ()=>{
+    formContainer.classList.add("show");
+})
+
+cross.addEventListener("click", ()=>{
+    formContainer.classList.remove("show");
+})
+
+// js for sign up page
+
+formContainer2 = document.querySelector(".form-container2");
+signUpA = document.querySelector(".signupA");
+cross2 = document.querySelector(".cross2");
+
+signUpA.addEventListener("click", ()=>{
+    formContainer2.classList.add("show");
+})
+
+cross2.addEventListener("click", ()=>{
+    formContainer2.classList.remove("show");
+})
 
   
